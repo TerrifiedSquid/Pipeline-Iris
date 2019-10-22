da6659a01c6d523cee575d4b75a05cade51b76e3
ArranHL <47016295+ArranHL@users.noreply.github.com>
2019-10-22 18:24:05 +0300

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,13 @@
+variable "notcount" {	
+  default = 1	
+}
+
+  variable "github_organization" {
+  description = "Organization Name"
+  default     = "TerrifiedSquid"
+}
+
+# The following variable is meant to point back to Github 
+variable "TOKEN" {
+  description = "Token for Access"
+}

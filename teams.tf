f312dd5a8f2622a48501f33f06756fee39018644
ArranHL <47016295+ArranHL@users.noreply.github.com>
2019-11-11 21:43:38 +0100

    Update teams.tf

--- teams.tf
+++ teams.tf
@@ -4,8 +4,8 @@
   } 
 
 # Example terraform command
- resource "github_membership" "ArranTest" {
- username = "ArranTest"
+ resource "github_membership" "dogi" {
+ username = "dogi"
  role = "member" 
  }
 
